8277e59b5d2c276c088cd54c9996e1923ebdc990
isdaft <ben.ashby2@gmail.com>
2020-01-18 16:14:26 -0500

    test

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,8 +4,8 @@ import history from '../history';
 
 const Modal = (props) => {
 	return ReactDOM.createPortal(
-			<div onClick={history.push('/')} className="ui dimmer modals visible active">
-				<div onClick={(e) => { e.stopPropagation()}} className="ui standard modal visible active">
+			<div className="ui dimmer modals visible active">
+				<div className="ui standard modal visible active">
 					<div className="header">
 						{props.title}
 					</div>
